8e45e7c95f4aab77c4829d57d6e6ae0e15a87e73
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-25 20:18:47 -0300

    Updated WhatIncludes.tsx

--- src/components/WhatIncludes.tsx
+++ src/components/WhatIncludes.tsx
@@ -113,9 +113,7 @@ const WhatIncludes = () => {
              </div>
             </div>
           </div>
-        </section>
-      );
-      };
+        
           {/* Botões de navegação */}
           <div className="flex flex-wrap gap-4 justify-center pt-8 mb-6">
             <a
