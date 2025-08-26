f0071368a51cfbadc34d5a010c032e3c41162463
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-26 20:37:20 -0300

    Updated FeaturesBook.tsx

--- src/components/FeaturesBook.tsx
+++ src/components/FeaturesBook.tsx
@@ -23,7 +23,7 @@ export default function FeaturesBook() {
             Fácil de entender
           </div>
 
-          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
+          <div className="inline-flex items-center justify-center px-7 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
             <BookOpen className="w-5 h-5 mr-2 text-pink-300" />
             100% Fiel à Bíblia
           </div>
