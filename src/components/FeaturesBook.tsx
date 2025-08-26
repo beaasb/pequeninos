35e9ef4bd5e922c574a0428295eb02cbef10d47b
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-26 20:51:28 -0300

    Updated FeaturesBook.tsx

--- src/components/FeaturesBook.tsx
+++ src/components/FeaturesBook.tsx
@@ -38,7 +38,7 @@ export default function FeaturesBook() {
       <section className="w-full py-12 bg-gradient-to-br from-purple-100 to-pink-100 text-gray-700">
         <div className="max-w-4xl mx-auto px-6 text-center">
           <h2 className="text-xl sm:text-2xl font-mediun leading-snug">
-            Nosso material irá transformar o aprendizado e leitura bíblica do seu filho!
+            Nosso material irá transformar o aprendizado e leitura bíblica do seu filho! {" "}
             <strong>Simples, Prático e Divertido.</strong>
           </h2>
         </div>
