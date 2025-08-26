a0dbe11cadfeecac85e6cdfa5ecc6577f5d55596
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-26 20:36:42 -0300

    Updated FeaturesBook.tsx

--- src/components/FeaturesBook.tsx
+++ src/components/FeaturesBook.tsx
@@ -13,7 +13,7 @@ export default function FeaturesBook() {
 
         {/* Lista de benefícios */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10 text-center">
-          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/40 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
+          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
             <Clock className="w-5 h-5 mr-2 text-yellow-300" />
             Leitura Rápida
           </div>
