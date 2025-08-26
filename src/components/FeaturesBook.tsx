083db4cf624d12392c4ab3440f525ea070986fa6
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-26 20:37:11 -0300

    Updated FeaturesBook.tsx

--- src/components/FeaturesBook.tsx
+++ src/components/FeaturesBook.tsx
@@ -18,17 +18,17 @@ export default function FeaturesBook() {
             Leitura Rápida
           </div>
 
-          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/10 text-white text-base font-medium backdrop-blur-sm shadow-sm">
+          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
             <CheckCircle className="w-5 h-5 mr-2 text-green-300" />
             Fácil de entender
           </div>
 
-          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/10 text-white text-base font-medium backdrop-blur-sm shadow-sm">
+          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
             <BookOpen className="w-5 h-5 mr-2 text-pink-300" />
             100% Fiel à Bíblia
           </div>
 
-          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/10 text-white text-base font-medium backdrop-blur-sm shadow-sm">
+          <div className="inline-flex items-center justify-center px-4 py-2 rounded-full bg-white/50 text-gray-700 text-base font-medium backdrop-blur-sm shadow-sm">
             <HandHeart className="w-5 h-5 mr-2 text-purple-300" />
             Proximidade de Deus
           </div>
