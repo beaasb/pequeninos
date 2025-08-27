997c3a797173bbfb761f8bd1a6bc820713bbb850
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-26 23:05:07 -0300

    Updated CompleteKit.tsx

--- src/components/CompleteKit.tsx
+++ src/components/CompleteKit.tsx
@@ -162,7 +162,7 @@ const CompleteKit = () => {
               <div className="flex flex-col items-center justify-center gap-3 mb-2">
                 <div className="text-2xl md:text-3xl font-bold text-green-200">
                   <span className="line-through">R$ 95,90</span>
-                  <span className="text-white"> …</span>
+                  <span className="text-white"> por</span>
                 </div>
                 <span className="text-5xl md:text-8xl font-bold text-white">
                   R$ 24,90
