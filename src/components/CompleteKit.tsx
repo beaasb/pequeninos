1d935a19086bb03ce23635dc63cf2283af62b973
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-27 18:31:37 -0300

    Updated CompleteKit.tsx

--- src/components/CompleteKit.tsx
+++ src/components/CompleteKit.tsx
@@ -94,7 +94,7 @@ const CompleteKit = () => {
               
               <li className="flex items-center text-xl mb-10"><Check className="w-5 h-5 mr-2 text-green-500" /> Livro Pequeninos - Colorindo com Jesus (em PDF)</li>
             </ul>
-            <h4 className="text-6xl font-bold mb-2 text-center">R$ 12,90</h4>
+            <h4 className="text-6xl font-bold mb-2 text-center text-green-600">R$ 12,90</h4>
             <p className="text-center text-gray-700 mb-4">ou 3x de R$6,52 no cartão</p>
             <div className="text-center">
               <a
