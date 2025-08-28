cdda7f72b3bb5edb3e5a95232b7ffa0d778339ec
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-28 15:21:57 -0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,10 +29,6 @@ const Hero = () => {
         </div>
 
         {/* Subhead */}
-
-        <p className="text-lg sm:text-3xl md:text-3xl text-gray-700 leading-relaxed font-semibold">
-          Você já tentou ensinar a Bíblia para as crianças e percebeu que, em poucos minutos, elas se distraem e perdem o interesse?
-        </p>
         <p className="text-lg sm:text-2xl md:text-2xl text-gray-700 leading-relaxed">
           Agora existe um jeito simples e divertido de aproximar os pequenos da Palavra de Deus:{" "}
           o livro da <strong>Pequeninos - Colorindo com Jesus</strong>, em PDF com entrega imediata para você imprimir quantas vezes quiser.{" "}
