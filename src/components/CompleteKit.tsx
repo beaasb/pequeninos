4f3a10f5398ea76ba3eeb4a70a0f6f21294125a0
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-28 16:12:10 -0300

    Updated CompleteKit.tsx

--- src/components/CompleteKit.tsx
+++ src/components/CompleteKit.tsx
@@ -216,7 +216,7 @@ const CompleteKit = () => {
                   height="60" 
                   src="https://i.imgur.com/iDMIGos.png" 
                   alt="Meios de pagamento" 
-                  class="w-full max-w-xs h-12 object-cover"/>
+                  class="w-full max-w-xs h-15 object-cover"/>
                 </div>              
             </div>
           </div>
