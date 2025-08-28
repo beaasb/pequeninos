a0301216260ddfdace6be45526f8d8866d9f6d71
Beatriz Bernardes <103227087+beaasb@users.noreply.github.com>
2025-08-28 17:31:39 -0300

    Updated SpoilerBook.tsx

--- src/components/SpoilerBook.tsx
+++ src/components/SpoilerBook.tsx
@@ -11,7 +11,7 @@ const SpoilerBook = () => {
     { image: "https://raw.githubusercontent.com/beaasb/pequeninos/refs/heads/main/images/XDZXarY.webp?token=GHSAT0AAAAAADJFZD7BWX7Z3VH34MZC5MDG2FQXR2A", alt: "Checklist de Progresso", title: "Checklist de Progresso" },
     { image: "https://raw.githubusercontent.com/beaasb/pequeninos/refs/heads/main/images/dqKEHrA.webp?token=GHSAT0AAAAAADJFZD7BYUL5GCBCTHJD7W2G2FQXSGQ", alt: "Sessões Temáticas", title: "2 Sessões Temáticas" },
     { image: "https://raw.githubusercontent.com/beaasb/pequeninos/refs/heads/main/images/mNUXXJo.webp?token=GHSAT0AAAAAADJFZD7ASPBA6CNMPLFXHY222FQXSWA", alt: "Sessões Temáticas", title: "3 Sessões Temáticas" },
-    { image: "images/D4SoMyJ%20-%20Imgur.webp", alt: "Atividades Criativas", title: "6 Atividades Criativas" },
+    { image: "https://raw.githubusercontent.com/beaasb/pequeninos/refs/heads/main/images/D4SoMyJ.webp?token=GHSAT0AAAAAADJFZD7BZD5IIJAP7YBEDIMM2FQXTLQ", alt: "Atividades Criativas", title: "6 Atividades Criativas" },
     { image: "https://github.com/beaasb/pequeninos/raw/refs/heads/main/images/8ulITpE%20-%20Imgur.webp", alt: "Certificado de Conclusão", title: "Certificado de Conclusão" },
   ];
 
